34dc6a07ceb584751d0e17d0ba92554904b2f128
İsmail Eski <ismaileski@gmail.com>
2024-12-13 01:02:04 +0300

    [UPD] Tax Template

--- l10n_tr_ex/models/template_tr_ex.py
+++ l10n_tr_ex/models/template_tr_ex.py
@@ -40,17 +40,17 @@ class AccountChartTemplate(models.AbstractModel):
     @template('tr_ex', 'account.account')
     def _get_tr_ex_account_account(self):
         tr_val = self._parse_csv('tr', 'account.account', module='l10n_tr')
-        tr_ex_val = self._parse_csv('tr_ex', 'account.account', module='l10n_tr_ex')
+        tr_ex_val = self._parse_csv('tr', 'account.account', module='l10n_tr_ex')
         return {**tr_val, **tr_ex_val}
 
     @template('tr_ex', 'account.tax.group')
     def _get_tr_ex_account_tax_group(self):
-        return self._parse_csv('tr_ex', 'account.tax.group', module='l10n_tr_ex')
+        return self._parse_csv('tr', 'account.tax.group', module='l10n_tr_ex')
 
     @template('tr_ex', 'account.tax')
     def _get_tr_ex_account_tax(self):
-        return self._parse_csv('tr_ex', 'account.tax', module='l10n_tr_ex')
+        return self._parse_csv('tr', 'account.tax', module='l10n_tr_ex')
 
     @template('tr_ex', 'account.fiscal.position')
     def _get_tr_ex_account_fiscal_position(self):
-        return self._parse_csv('tr_ex', 'account.fiscal.position', module='l10n_tr_ex')
+        return self._parse_csv('tr', 'account.fiscal.position', module='l10n_tr_ex')
