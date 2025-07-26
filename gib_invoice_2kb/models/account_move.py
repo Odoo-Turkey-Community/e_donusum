aed7e644d4e3bd89e0b69c1e82f918e37e32fd05
Ömer KÜLAK <omer.kulak@kitayazilim.com>
2025-07-26 18:29:58 +0300

    mail ile dijital faturanın gönderilmesi

--- gib_invoice_2kb/models/account_move.py
+++ gib_invoice_2kb/models/account_move.py
@@ -1010,9 +1010,17 @@ class AccountMove(models.Model):
                     "name": attachment_name,
                     "res_model": "account.move",
                     "res_id": self.id,
+                    # "res_field": "invoice_pdf_report_file"
                     "type": "binary",
                     "mimetype": "application/pdf",
                     "datas": base64.b64encode(pdf_result),
                 }
             )
         return pdf_result
+
+    def _get_invoice_report_filename(self, extension="pdf"):
+        res = super()._get_invoice_report_filename(extension=extension)
+        if self.gib_profile_id and self.gib_invoice_name and self.gib_state == "sent":
+            # If the invoice name is the default, use the move name instead
+            return f"{self.gib_invoice_name}_{self.gib_uuid}.{extension}"
+        return res
