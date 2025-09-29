c89e95a812baf1f2a118312617448d569cc2651b
mehmet <mehmet.gulsoy@kitayazilim.com>
2025-09-29 15:39:01 +0300

    [UPD] GIB ön eki ile fatura kesilmesi sistemimiz tarafından engellenecektir.

--- gib_base_2kb/models/gib_user.py
+++ gib_base_2kb/models/gib_user.py
@@ -18,7 +18,7 @@ class GibUserAlias(models.Model):
 
         domain = domain or []
         for item in domain:
-            if isinstance(item, list) and item[0] == 'vkn_tckn' and item[2][:2].isalpha():
+            if isinstance(item, list) and item[0] == 'vkn_tckn' and item[2] and item[2][:2].isalpha():
                 item[2] = item[2][2:]
         return super()._name_search(name, domain, operator, limit, order)
 
