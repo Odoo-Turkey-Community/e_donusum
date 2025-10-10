f2c7968b3e87b16cbc705ffbd11844c7d5edbb8a
Ömer KÜLAK <omer.kulak@kitayazilim.com>
2025-10-10 08:38:09 +0300

    readme düzeltmeleri

--- kita_api_services/__manifest__.py
+++ kita_api_services/__manifest__.py
@@ -3,16 +3,15 @@
     "name": "Kıta Yazılım API Services",
     "version": "1.4",
     "summary": """ Kıta Yazılım API Key ve Secret saklama""",
-    "author": "Kıta Yazılım",
-    "website": "kitayazilim.com",
+    "author": "Kıta",
+    "maintainer": "Kıta Yazılım",
+    "website": "https://kitayazilim.com",
+    "support": "support@kitayazilim.com",
     "category": "Technical",
     "depends": ["base", "base_setup"],
-    "data": [
-        "views/res_company_views.xml",
-        "views/res_config_settings_views.xml"
-    ],
+    "data": ["views/res_company_views.xml", "views/res_config_settings_views.xml"],
     "application": False,
     "installable": True,
     "auto_install": False,
-    "license": "LGPL-3"
+    "license": "LGPL-3",
 }
